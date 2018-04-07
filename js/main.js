e6a47dbce8bb1fb43c891139799341c68f4b7a4c
atom-web <olologvinov.o@gmail.com>
2018-04-08 02:02:28 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.grid').masonry({
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 0,
+});
+
+
+$('.slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+
