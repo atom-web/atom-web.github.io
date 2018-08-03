555d1323347305fa3bc231ae15826f7010544ce3
atom-web <olologvinov.o@gmail.com>
2018-08-04 00:15:55 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $('.multiple-items').slick({
   infinite: true,
   arrows: false,
-  autoplay: false,
+  autoplay: true,
   slidesToShow: 1,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+  fade: true,
 });
 
 $( document ).ready(function() {
@@ -11,3 +12,26 @@ $( document ).ready(function() {
     $(this).find("img").toggleClass('img-ban-active');
   });
 });
+$( document ).ready(function() {
+    $('.sale-box').hover(function(){
+    $(this).find("img").toggleClass('img-ban-active');
+    $('.sale-banner-top').toggleClass('sale-banner-active');
+    
+    
+    if ($('.sale-banner-top').hasClass('sale-banner-active')) {
+    	$('.sale-banner').removeClass('fadeInDown');
+    	$('.sale-banner').addClass('fadeOutDown');
+    	$('.sale-banner-top').removeClass('fadeOutLeft');
+    	$('.sale-banner-top').addClass('fadeInRight');
+    } 
+    else {
+    	$('.sale-banner').addClass('fadeInDown');
+    	$('.sale-banner').removeClass('fadeOutDown');
+    	$('.sale-banner-top').addClass('fadeOutLeft');
+    	$('.sale-banner-top').removeClass('fadeInRight');
+    };
+
+  });
+});
+
+
