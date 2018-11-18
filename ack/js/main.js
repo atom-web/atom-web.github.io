3688b26f190fdc7f9f8562d9a26db5f6abbd3c53
Oleg <olologvinov.o@gmail.com>
2018-11-18 03:40:29 +0300

    ack правки формы

--- ack/js/main.js
+++ ack/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    // Обработчик Формы_________________________________
     $(".header__search-link").click(function(){
 
         $(".header__search-form").toggleClass('form-active');
@@ -11,9 +13,17 @@ $(document).ready(function(){
                 $(".header__search-form").removeClass('bounceInRight');
                 $(".header__search-form").addClass('bounceOutRight');
 
-    }
+        }
     });
-    $("body").click(function(){
-        
+    $(document).mouseup(function (e){
+        var div = $(".header__search-form, .header__search-link"); // class или ID элемента которого обрабатываем
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+
+            if ($('.header__search-form').hasClass('form-active')) {
+                 $(".header__search-form").addClass('bounceOutRight');
+                 $(".header__search-form").removeClass('bounceInRight form-active');
+            }
+        }
     });
 });
