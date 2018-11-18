ef68c6ee5c38ae2f28b623c95965471dedc0df11
Oleg <olologvinov.o@gmail.com>
2018-11-18 23:51:36 +0300

    slider company

--- ack/js/main.js
+++ ack/js/main.js
@@ -27,10 +27,12 @@ $(document).ready(function(){
     //     }
     // });
     $('.company-slide').slick({
-      infinite: true,
       slidesToShow: 7,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
+      arrows: true,
+      nextArrow: '<div class="next-arrows"><i class="fas fa-chevron-right"></i></div>',
+      prevArrow: '<div class="prev-arrows"><i class="fas fa-chevron-left"></i></div>',
   });
 });
