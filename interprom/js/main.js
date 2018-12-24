fd3e6b80136c2e0a94780f2d2d60240cc5cbd497
Oleg <olologvinov.o@gmail.com>
2018-12-24 17:42:26 +0300

    interprom katalog add

--- interprom/js/main.js
+++ interprom/js/main.js
@@ -1,18 +1,46 @@
 $(document).ready(function(){
-	$('.slider__init').slick({
+	$('.video-feat__slider').slick({
 		infinite: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		fade: true,
+		fade: false,
 		cssEase: 'linear',
-		autoplay: true,
+		autoplay: false,
 		autoplaySpeed: 2000,
 		arrows: true,
 		dots: true,
-		nextArrow: '<div class="next-arrows"><img src="img/right-arrow.svg" alt=""></div>',
-		prevArrow: '<div class="prev-arrows"><img src="img/right-arrow.svg" alt=""></div>',
+		nextArrow: '<div class="next-arrows"><i class="fas fa-angle-right"></i></div>',
+		prevArrow: '<div class="prev-arrows"><i class="fas fa-angle-right"></i></div>',
 		customPaging: (function(slider, i) {return '<div class="slider__slick-dots"></div>';}),
 	});
+    $('.slider__init').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        nextArrow: '<div class="next-arrows"><img src="img/right-arrow.svg" alt=""></div>',
+        prevArrow: '<div class="prev-arrows"><img src="img/right-arrow.svg" alt=""></div>',
+        customPaging: (function(slider, i) {return '<div class="slider__slick-dots"></div>';}),
+    });
+    $('.img-feat__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: false,
+        cssEase: 'linear',
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: true,
+        dots: true,
+        nextArrow: '<div class="next-arrows"><i class="fas fa-angle-right"></i></div>',
+        prevArrow: '<div class="prev-arrows"><i class="fas fa-angle-right"></i></div>',
+        customPaging: (function(slider, i) {return '<div class="slider__slick-dots"></div>';}),
+    });
 
 
 // Преобразование img в svg______________________________________
@@ -46,6 +74,7 @@ $(document).ready(function(){
 
     // Инициализация маски ввода телефона для формы_____________________
     $(".phone_mask").mask("+7(999)999-99-99", {autoclear: false});
+    $(".popup-tel-mask").mask("+7(999)999-99-99", {autoclear: false});
 
     // Открытие меню____________________________________________________
     $(".link-open-menu").click(function(){
@@ -61,4 +90,55 @@ $(document).ready(function(){
         $("body").removeClass('popup-active');
     });
 
-});
+    //Смена стилей плитки
+    if ($('.block-change__item, .list-change__item').length > 0) {
+        $('.block-change__item, .list-change__item').click(function(e) {
+            e.preventDefault();
+
+            $('.block-change__item, .list-change__item').removeClass('block-change__active');
+            $(this).addClass('block-change__active');
+
+            if ($('.list-change__item').hasClass('block-change__active')) {
+                $('.catalog__item').addClass('catalog__active-list');
+            } else {
+                $('.catalog__item').removeClass('catalog__active-list');
+            }
+        });
+    };
+
+
+    var stickySidebar = $('.sticky');
+
+    if (stickySidebar.length > 0) { 
+      var stickyHeight = stickySidebar.height(),
+          sidebarTop = stickySidebar.offset().top;
+    }
+
+    // on scroll move the sidebar
+    $(window).scroll(function () {
+      if (stickySidebar.length > 0) {   
+        var scrollTop = $(window).scrollTop();
+                
+        if (sidebarTop < scrollTop) {
+          stickySidebar.css('top', scrollTop - sidebarTop);
+
+          // stop the sticky sidebar at the footer to avoid overlapping
+          var sidebarBottom = stickySidebar.offset().top + stickyHeight,
+              stickyStop = $('.main-content').offset().top + $('.main-content').height();
+          if (stickyStop < sidebarBottom) {
+            var stopPosition = $('.main-content').height() - stickyHeight;
+            stickySidebar.css('top', stopPosition);
+          }
+        }
+        else {
+          stickySidebar.css('top', '0');
+        } 
+      }
+    });
+
+    $(window).resize(function () {
+      if (stickySidebar.length > 0) {   
+        stickyHeight = stickySidebar.height();
+      }
+    });
+}); 
