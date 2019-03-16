63e350941ff921587be299dec08a56e488c097c3
Oleg <olologvinov.o@gmail.com>
2019-03-16 17:40:45 +0300

    bambook

--- bambook/js/main.js
+++ bambook/js/main.js
@@ -36,9 +36,11 @@ $( document ).ready(function() {
             $('.header-search__input label').addClass('header-search__input-txt')
         }
     });
-    $('.work__item:not(:last)').hover(function(){
-        $('.work-info').toggleClass('work-info-anim')
-    });
+    if ($(window).width() > 991) {
+        $('.work__item:not(:last)').hover(function(){
+            $('.work-info').toggleClass('work-info-anim')
+        });
+    }
 
     //Ajax отправка формы
     $(function() {
@@ -80,4 +82,49 @@ $( document ).ready(function() {
     $('.form-call__inp-tel').mask('+7 (000) 000-00-00', {placeholder: "+7 (999) 000-00-00"});
 
     $('.top-prew__bg').css('transform', 'scale(1.5)');
+
+    if ($(window).width() > 1200) {
+        var anchors = [];
+        var currentAnchor = -1;
+        var isAnimating  = false;
+
+        $(function(){
+            
+            function updateAnchors() {
+                anchors = [];
+                $('.anchor').each(function(i, element){
+                    anchors.push( $(element).offset().top );
+                });
+            }
+            
+            $('body').on('mousewheel', function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                if( isAnimating ) {
+                    return false;
+                }
+                isAnimating  = true;
+                // Increase or reset current anchor
+                if( e.originalEvent.wheelDelta >= 0 ) {
+                    currentAnchor--;
+                }else{
+                    currentAnchor++;
+                }
+                if( currentAnchor > (anchors.length - 1) 
+                   || currentAnchor < 0 ) {
+                    currentAnchor = 0;
+                }
+                isAnimating  = true;
+                $('html, body').animate({
+                    scrollTop: parseInt( anchors[currentAnchor] )
+                }, 500, 'swing', function(){
+                    isAnimating  = false;
+                });
+            });
+
+            updateAnchors();   
+            
+        });
+    }
+    
 });
