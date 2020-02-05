bbd1635b6d5dc4ab39ba99d8862aa6904f200f90
Oleg Logvinov <olologvinov.o@gmail.com>
2020-02-05 21:35:03 +0300

    wattson, верхний хедер

--- wattson/js/main.js
+++ wattson/js/main.js
@@ -14,3 +14,20 @@ $('img[src$=".svg"]').each(function() {
         $img.replaceWith($svg);
     }, 'xml'); 
 });
+
+
+//Присваивание города в попап окно "popup-locat__select-city"
+var locPopup = $('.popup-locat__select-city'); //место вставки города
+var topCity = $('.head-login__top-city'); //город который нужно вставить
+var titleLoc = $('.popup-locat__item-title'); //список городов в попап окне
+
+if (locPopup.length) {
+    var locCity = topCity.html();
+    locPopup.html(locCity)
+}
+titleLoc.each(function(i, e){
+    if ($(this).html() == topCity.html()) {
+        $(this).parent().addClass('locat-active') //активный класс на город в попап окне
+        $(this).parent().attr('onclick', 'return false'); //запрет стандартного поведения кнопки
+    }
+});
